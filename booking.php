10b55f1dbaeccb03f0058ff087184f0aaa73e797
B Jayodha <76960054+BeminduJayodha@users.noreply.github.com>
2025-05-27 13:49:55 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,10 +1,4 @@
 <?php   
-/**
- * Plugin Name: Booking Calendar Plugin
- * Description: A plugin for admin to book time slots and display them with colors.
- * Version: 1.0
- * Author: makerspace
- */
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
